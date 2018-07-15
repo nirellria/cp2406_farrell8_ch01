1317d46cdf1a2f1b47204a67d725d41191ac5a6c
nirellria <nirelleunice.ria@my.jcu.edu.au>
2018-07-15 19:49:30 +0800

    Movie quote

---
+++ prac_01/MovieQuote.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nirellria on 15/07/18.
+ */
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("Somehow the silence seemed to connect us in a way like words never could.");
+    }
+}
