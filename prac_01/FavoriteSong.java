5745aef347bf979816299edc891998961585c1ad
nirellria <nirelleunice.ria@my.jcu.edu.au>
2018-07-15 19:54:17 +0800

    Favorite Song

---
+++ prac_01/FavoriteSong.java
@@ -0,0 +1,11 @@
+/**
+ * Created by nirellria on 15/07/18.
+ */
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Sudah terlalu lama sendiri.");
+        System.out.println("Sudah terlalu lama aku asik sendiri.");
+        System.out.println("Lama tak ada yang menemani.");
+        System.out.println("Rasanya.");
+    }
+}
